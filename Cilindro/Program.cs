34e514a18f502e7d2b59ff4d9588507bac17074a
Joana Marques <joana.pipaa@gmail.com>
2018-02-22 17:28:17 +0000

    Primeiro commit

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a altura do cilindro");
+            string a = Console.ReadLine();
+            Console.WriteLine("Insira o raio do cilindro");
+            string r = Console.ReadLine();
+        }
+    }
+}
